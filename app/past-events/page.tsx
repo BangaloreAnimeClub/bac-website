75fd67a37901e7940860ab80039f18c82d022427
Aravind G <aravindg1@iisc.ac.in>
2024-10-27 10:34:44 +0530

    remove tags from events pages

--- app/past-events/page.tsx
+++ app/past-events/page.tsx
@@ -29,17 +29,20 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     POSTS_PER_PAGE * currentPage
   );
 
-  const tags = getAllTags(pastEventsPosts);
-  const sortedTags = sortTagsByCount(tags);
+  // const tags = getAllTags(pastEventsPosts);
+  // const sortedTags = sortTagsByCount(tags);
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Past Events</h1>
+          <p className="text-xl text-muted-foreground">
+            Check out our past events
+          </p>
         </div>
       </div>
-      <div className="grid grid-cols-12 gap-3 mt-8">
+      <div className="grid gap-3 mt-8">
         <div className="col-span-12 col-start-1 sm:col-span-8">
           <hr />
           {displayPosts?.length > 0 ? (
@@ -53,32 +56,24 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
                       date={date}
                       title={title}
                       description={description}
-                      tags={tags}
+                      // tags={tags}
                     />
                   </li>
                 );
               })}
             </ul>
           ) : (
-            <p className="text-xl text-muted-foreground">
-            Nothing to see here yet!
-          </p>
+            <div className="mt-8">
+              <p className="text-xl text-muted-foreground">
+                Nothing to see here yet!
+              </p>
+            </div>
           )}
           <QueryPagination
             totalPages={totalPages}
-            className="justify-end mt-4"
+            className="mt-4"
           />
         </div>
-        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
-          <CardHeader>
-            <CardTitle>Tags</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-wrap gap-2">
-            {sortedTags?.map((tag) => (
-              <Tag tag={tag} key={tag} count={tags[tag]} />
-            ))}
-          </CardContent>
-        </Card>
       </div>
     </div>
   );
--- app/upcoming-events/page.tsx
+++ app/upcoming-events/page.tsx
@@ -29,8 +29,8 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     POSTS_PER_PAGE * currentPage
   );
 
-  const tags = getAllTags(upcomingEventsPosts);
-  const sortedTags = sortTagsByCount(tags);
+  // const tags = getAllTags(upcomingEventsPosts);
+  // const sortedTags = sortTagsByCount(tags);
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
@@ -42,7 +42,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-12 gap-3 mt-8">
+      <div className="grid gap-3 mt-8">
         <div className="col-span-12 col-start-1 sm:col-span-8">
           <hr />
           {displayPosts?.length > 0 ? (
@@ -56,30 +56,24 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
                       date={date}
                       title={title}
                       description={description}
-                      tags={tags}
+                      // tags={tags}
                     />
                   </li>
                 );
               })}
             </ul>
           ) : (
-            <p>Nothing to see here yet!</p>
+            <div className="mt-8">
+              <p className="text-xl text-muted-foreground">
+                Nothing to see here yet!
+              </p>
+            </div>
           )}
           <QueryPagination
             totalPages={totalPages}
-            className="justify-end mt-4"
+            className="mt-4"
           />
         </div>
-        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
-          <CardHeader>
-            <CardTitle>Tags</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-wrap gap-2">
-            {sortedTags?.map((tag) => (
-              <Tag tag={tag} key={tag} count={tags[tag]} />
-            ))}
-          </CardContent>
-        </Card>
       </div>
     </div>
   );
