3c6730c3fc1ade917934d9e680b107b306ac1edf
Aravind G <aravindg1@iisc.ac.in>
2024-10-29 15:21:55 +0530

    fa social icons

--- app/socials/page.tsx
+++ app/socials/page.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { siteConfig } from "@/config/site";
 import { Metadata } from "next";
 import Link from "next/link";
-
+import { FaSquareInstagram, FaSquareWhatsapp, FaSquareXTwitter, FaDiscord, FaFacebook, FaTelegram } from "react-icons/fa6";
 
 export const metadata: Metadata = {
   title: "BAC · Socials",
@@ -48,57 +48,49 @@ export default async function SocialsPage() {
         
         <li style={linkStyle}>
           <a href="https://instagram.com/bangaloreanimeclub/" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/instagram.png" alt="Instagram" style={iconStyle} />
-            <span style={textStyle}>Instagram</span>
+            <FaSquareInstagram size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Instagram</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://x.com/BLRAnimeClub" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/twitter.png" alt="X (Twitter)" style={iconStyle} />
-            <span style={textStyle}>Twitter</span>
+            <FaSquareXTwitter size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Twitter</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://whatsapp.com/channel/0029Va4WaK1I1rcnkEFOnV35" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/whatsapp.png" alt="WhatsApp" style={iconStyle} />
-            <span style={textStyle}>WhatsApp (Channel)</span>
+            <FaSquareWhatsapp size = {30}/>&nbsp;&nbsp;<span style={textStyle}>WhatsApp (Channel)</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://chat.whatsapp.com/FoLaQKZc7Lq3gdPVkkOIZJ" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/whatsapp.png" alt="WhatsApp" style={iconStyle} />
-            <span style={textStyle}>WhatsApp (Community)</span>
+            <FaSquareWhatsapp size = {30}/>&nbsp;&nbsp;<span style={textStyle}>WhatsApp (Community)</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://discord.gg/EumfnhyZvB" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/discord.png" alt="Discord" style={iconStyle} />
-            <span style={textStyle}>Discord</span>
+            <FaDiscord size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Discord</span>
           </a>
         </li>
         
         <li style={linkStyle}>
           <a href="https://www.facebook.com/bangaloreanimeclub" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/facebook.png" alt="Facebook" style={iconStyle} />
-            <span style={textStyle}>Facebook (Page)</span>
+            <FaFacebook size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Facebook (Page)</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://facebook.com/groups/BangaloreAnimeClub/" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/facebook.png" alt="Facebook" style={iconStyle} />
-            <span style={textStyle}>Facebook (Group)</span>
+            <FaFacebook size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Facebook (Group)</span>
           </a>
         </li>
 
         <li style={linkStyle}>
           <a href="https://t.me/joinchat/U4PbJ5tLTovcFNnz" target="_blank" style={{ display: 'flex', alignItems: 'center' }}>
-            <img src="images/socials/telegram.png" alt="Telegram" style={iconStyle} />
-            <span style={textStyle}>Telegram</span>
+            <FaTelegram size = {30}/>&nbsp;&nbsp;<span style={textStyle}>Telegram</span>
           </a>
         </li>
 
